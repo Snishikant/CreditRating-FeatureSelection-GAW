9ac11fc52df8b39d93fd5b4121481b22a6908f6d
Snishikant <singhnishikant0@gmail.com>
2018-09-16 11:50:38 +0530

    dataset required for the project along with pre-processing

---
+++ dataset/test.py
@@ -0,0 +1,15 @@
+#use this to convert .dat to .csv
+'''import csv
+
+aus = [i.strip().split(',') for i in open("./australian.dat").readlines()]
+with open("./australian.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(aus)
+'''
+
+#use it read from csv
+from __future__ import print_function 
+import pandas as pd
+
+aus = pd.read_csv('australian.csv')
+print (aus)
